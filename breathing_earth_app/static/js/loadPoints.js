f9460cc58acc254e3fa9e98c8f535964f69189e2
13scoobie <13scoobie@gmail.com>
2016-05-16 21:25:48 -0700

    updated functions to load data, switched data files, updated css, started adding data tables. next step include data tables and update with crossfilter

--- breathing_earth_app/static/js/loadPoints.js
+++ breathing_earth_app/static/js/loadPoints.js
@@ -1,4 +1,5 @@
-    var citymap = {};
+    var coordlist = {};
+    var datelist = {};
 
     function initMap() {
         // Create the map.
@@ -12,13 +13,51 @@
             mapTypeId: google.maps.MapTypeId.TERRAIN
         });
 
-         loadJSON(function(response) {
+
+        loadInitialPoints(function(response) {
+            coordlist = JSON.parse(response);
+            //console.log(coordlist);
+            var datatablecount = 0;
+            
+            for (var loc in coordlist) {
+                //console.log('loc: ' + loc +' lat: '+coordlist[loc].lat + ' lng: '+coordlist[loc].lng);
+                // Add the circle for this city to the map.
+                var cityCircle = new google.maps.Circle({
+                    strokeColor: '#FF0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35,
+                    map: map,
+                    center: coordlist[loc], //.coordinates,
+                    radius: 10000 //Math.sqrt(citymap[city].adj_du) * 100000
+                });
+                if(datatablecount <= 10) {
+                    $('#datatable').append("<tr><td>"+loc+"</td><td>"+coordlist[loc].lat+"</td><td>"+coordlist[loc].lng+"</td></tr>");
+                    datatablecount++;
+                }
+                
+            }
+            
+
+        });      
+
+
+        /*loadChanges(function(response) {
           // Parse JSON string into object
-            citymap = JSON.parse(response);
-            console.log(citymap);
+            datelist = JSON.parse(response);
+            console.log(datelist);
 
             // Construct the circle for each value in citymap.
-            for (var city in citymap) {
+            for (var dates in datelist) {
+                console.log(dates);
+                for(var sat in datelist[dates]) {
+                    console.log('sat: '+sat + ' | value: '+datelist[dates][sat]);
+                }
+            };
+        });
+        */
+         /*
                 // Add the circle for this city to the map.
                 var cityCircle = new google.maps.Circle({
                     strokeColor: '#FF0000',
@@ -32,14 +71,29 @@
                 });
             }
         });
+        */
     }
 
     //TODO: Could replace with jQuery if we decide to use, dont have to
-    function loadJSON(callback) {
+    function loadInitialPoints(callback) {
         
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-        xobj.open('GET', 'static/data/data2.json', true); // Replace 'my_data' with the path to your file
+        xobj.open('GET', 'static/data/coordinates.json', true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function() {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+            }
+        };
+        xobj.send(null);
+    }
+
+    function loadChanges(callback) {
+        
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', 'static/data/positions_sample_size_30_pnw.json', true); // Replace 'my_data' with the path to your file
         xobj.onreadystatechange = function() {
             if (xobj.readyState == 4 && xobj.status == "200") {
                 // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
